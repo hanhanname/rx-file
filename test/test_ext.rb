778d751c55eeffa3807e777fb2bc683693aaf4fa
Torsten Ruger <torsten@villataika.fi>
2015-05-15 20:59:22 +0300

    add test for new feature

---
+++ test/test_ext.rb
@@ -0,0 +1,31 @@
+require_relative "helper"
+
+class FailValue
+  def initialize str
+    @name = str
+  end
+  def is_value?
+    true
+  end
+end
+class BasicValue < FailValue
+  def to_sof
+    "'#{@name}'"
+  end
+end
+
+class ObjectSof < MiniTest::Test
+  include Checker
+
+  def test_to_sof
+    assert_raises NoMethodError do
+      Sof::Writer.write(FailValue.new("name"))
+    end
+  end
+
+  def test_basic
+    @out = Sof::Writer.write(BasicValue.new("name"))
+    check "'name'"
+  end
+
+end
