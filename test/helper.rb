fbae6a02764dbe97e01e4833f9ffffe09879b100
Torsten Ruger <torsten@villataika.fi>
2015-07-18 12:59:32 +0300

    bundler is back, instructions must ahve been for rspec

--- test/helper.rb
+++ test/helper.rb
@@ -1,4 +1,12 @@
-require "rubygems"
+require 'rubygems'
+require 'bundler'
+begin
+  Bundler.setup(:default, :development)
+rescue Bundler::BundlerError => e
+  $stderr.puts e.message
+  $stderr.puts "Run `bundle install` to install missing gems"
+  exit e.status_code
+end
 if ENV['CODECLIMATE_REPO_TOKEN']
   require "codeclimate-test-reporter"
   CodeClimate::TestReporter.start
